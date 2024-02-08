fc2b711302627bb81168687fa78175599daec30e
avinashtechlvr <avinashiniiitdwd@gmail.com>
2024-02-08 08:09:22 +0530

    Updated notes look for list display

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -33,16 +33,16 @@ export const Notes: React.FC<NoteProps> = ({ note }) => {
     //   </div>
     // </div>
    
-      <div className="relative p-2 transform transition duration-500 hover:scale-110 w-full max-w-xs md:max-w-sm lg:max-w-md xl:max-w-lg">
+      <div className=" relative p-2 transform transition duration-500 hover:scale-110 w-80 m-4">
         <div className="bg-gray-100 dark:bg-gray-700 cursor-pointer text-center shadow-md hover:shadow-lg p-4 rounded-lg w-full" onClick={handleOpenPopup}>
           <button onClick={handleDelete} className="absolute top-2 right-2 m-2 text-red-500 hover:text-red-700 transition-colors duration-300 ">
             <TrashIcon />
           </button>
-          <h1 className="font-bold dark:text-gray-300 text-xl uppercase text-[#ffab4a] flex items-center justify-center mt-4">
-            <BiNotepad className="mr-2 text-[#ffab4a]" />
+          <h1 className="font-bold dark:text-gray-300 text-xl uppercase text-[#ffab4a] flex items-start justify-start mt-4">
+            <BiNotepad className="ml-2 mt-1 mr-3 text-[#ffab4a]" />
             {note.title}
           </h1>
-          <p className="dark:text-gray-300 ml-5">{note.content}</p>
+          <p className="dark:text-gray-300 mt-4 mb-5 ml-5">{note.content}</p>
         </div>
       </div>
 
